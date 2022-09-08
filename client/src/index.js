d6c275a16b2f533b784167769ee55bab3c5baaea
sidhu-22 <sidharthsai1998@gmail.com>
2022-09-08 17:44:16 +0530

    Welcome to food app

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.js'
 
-import "bootstrap-icons/font/bootstrap-icons.css"
+// import "bootstrap-icons/font/bootstrap-icons.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
